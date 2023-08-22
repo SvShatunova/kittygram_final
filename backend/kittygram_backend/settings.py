6d261466d4d9fa8b0a4fc646dd34e0fd5f92bd71
svshatunova <svshatun0va@yandex.ru>
2023-08-22 16:01:02 +0300

    kittygram

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,16 +1,18 @@
 # flake8: noqa
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-from django.core.management.utils import get_random_secret_key
+load_dotenv()
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent 
 
-SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = False
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
+DEBUG = os.getenv('DEBUG', 'False').lower == 'true'
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
